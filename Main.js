14c6bd19d015066fb13a6337a36bca940b8a91ac
Nicat Güliyev <ni.guliyev@ady.az>
2026-06-16 10:15:54 +0400

    Main.js tamalandi

--- Main.js
+++ Main.js
@@ -1,41 +1,96 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import AuthLoginPage from './src/pages/AuthLoginPage';
-import {createNativeStacknavigator} from '@react-navigation/native-stack';
-import {inject, observer} from 'mobx-react';
-import {Platform} from 'react-native';
-import {useAdyStore} from './src/store/ady';
-import api, {$axios} from './src/api/api';
+import { createNativeStackNavigator} from '@react-navigation/native-stack';
+import { inject, observer } from 'mobx-react';
+import { Platform } from 'react-native';
+import { useAdyStore } from './src/store/ady';
+import api, { $axios } from './src/api/api';
 import ForceUpdatePage from './src/pages/ForceUpdatePage';
 import DeviceInfo from 'react-native-device-info';
-import {compareVersions} from 'compare-versions';
+import { compareVersions } from 'compare-versions';
 import Loading from './src/components/Loading';
-import OnBoarding  from './src/pages/OnBoarding';
+import OnBoarding from './src/pages/OnBoarding';
 
 
-const Stack = createNativeStacknavigator();
-const currentAppversion = DeviceInfo.getVersion();
+const Stack = createNativeStackNavigator();
+const currentAppVersion = DeviceInfo.getVersion();
 
-const SubMain  = observer(({user, initialRoute}) => {
-   const ady = useAdyStore();
+const SubMain = observer(({ user, initialRoute }) => {
+    const ady = useAdyStore();
 
-   useEffect(() => {
+    useEffect(() => {
         ady.setInitialRoute(initialRoute);
-   }, []);
+    }, []);
 
-   const compareVersionValue = !!ady.getAppVersionFromApi() && 
-   compareVersions(currentAppVersion, ady.getAppVersionFromApi()?.version) === -1;
+    const compareVersionValue = !!ady.getAppVersionFromApi() &&
+        compareVersions(currentAppVersion, ady.getAppVersionFromApi()?.version) === -1;
 
-   return (
-    <Stack.Navigator
-    useLegacyImplementation = {true}
-    screenOptions = {{headerShown: false,}}
-    initialRouteName = {compareVersionValue ? 'ForceUpdatepage' : initialRoute}
-    >
-    <Stack.Screen name = "Login" component = {AuthLoginPage} />
-    <Stack.Screen name = "OnBoarding" component = {OnBoarding} />
-    <Stack.Screen name = "ForceUpdatepage" component = {ForceUpdatePage} />
-    </Stack.Navigator>
-   );
+    return (
+        <Stack.Navigator
+            useLegacyImplementation={true}
+            screenOptions={{ headerShown: false, }}
+            initialRouteName={compareVersionValue ? 'ForceUpdatePage' : initialRoute}
+        >
+            <Stack.Screen name="Login" component={AuthLoginPage} />
+            <Stack.Screen name="OnBoarding" component={OnBoarding} />
+            <Stack.Screen name="ForceUpdatePage" component={ForceUpdatePage} />
+        </Stack.Navigator>
+    );
 });
+
+const Main = ({ user, initialRoute }) => {
+    const ady = useAdyStore();
+    const [userLoaded, setUserLoaded] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                await user.initUserFromStorage();
+                const response = await $axios.get(api.app_version);
+
+                const appVersionObj = Platform.OS === 'ios' ? response.data.ios : response.data.android;
+
+                if (Platform.OS === 'ios') {
+                    ady.setAppVersion(appVersionObj);
+                }
+                else {
+                    ady.setAppVersion(appVersionObj);
+                }
+            }
+            catch (e) {
+            }
+            finally {
+                setUserLoaded(true);
+            }
+        })();
+    }, []);
+
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        if (user.isSuccessReceiptVisible) {
+            navigation.navigate('SuccessReceiptPage');
+        }
+    }, [user?.isSuccessReceiptVisible]);
+
+
+    if (userLoaded) {
+        return <SubMain user={user} initialRoute={initialRoute} />
+    }
+
+
+    return <Loading loading={true} />
+
+};
+
+export default inject('user')(observer(Main));
+
+
+
+
+
+
+
+
 
